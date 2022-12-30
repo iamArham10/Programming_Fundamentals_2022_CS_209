bd2fd2315dbaf8468644edbdd620fdfce1915aff
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2022-12-30 14:58:43 +0500

    Update Task_14.cpp

--- PF_week_04_Lab_Friday/Task_14.cpp
+++ PF_week_04_Lab_Friday/Task_14.cpp
@@ -9,7 +9,7 @@ void compare(string std1, int ecat_1,string std2,int ecat_2);
 main() {
 string std1,std2;
 float fscMarks_1,fscMarks_2,matricMarks_1,matricMarks_2,ecatmarks_1,ecatmarks_2;
-printMenu()
+printMenu();
 cout<<"Enter name of student 1: ";
 cin >> std1;
 cout<<"Enter your fsc marks: ";
@@ -20,7 +20,7 @@ cout<<"Enter ecat marks: ";
 cin >> ecatmarks_1;
 
 cout<<"Enter name of student 2: ";
-cin >> std1;
+cin >> std2;
 cout<<"Enter your fsc marks: ";
 cin >> fscMarks_2;
 cout<<"Enter your matric marks: ";
@@ -32,6 +32,7 @@ calculateAggregate(std1,matricMarks_1,fscMarks_1,ecatmarks_1);
 cout<<endl;
 cout<<"student two ahs aggregate: ";
 calculateAggregate(std2,matricMarks_2,fscMarks_2,ecatmarks_2);
+cout<<endl;
 compare(std1,ecatmarks_1,std2,ecatmarks_2);
 
 }
@@ -47,8 +48,8 @@ float aggregate = (matricMarks/1100)*30 + (fscMarks/550)*30 + (ecatMarks/400)*40
 cout<<aggregate;
 }
 void compare(string std1, int ecat_1,string std2,int ecat_2){
-if (ecat_1 > ecat_2) {cout<<std1<<"has roll no 1 and "<<std2<<" has roll no 2"<<endl;}
-if (ecat_2 > ecat_1) {cout<<std2>>"has roll no 1 and "<<std1<<" has roll no 2"<<endl;}
+if (ecat_1 > ecat_2) {cout<<std1<<" has roll no 1 and "<<std2<<" has roll no 2"<<endl;}
+if (ecat_2 > ecat_1) {cout<<std2<<" has roll no 1 and "<<std1<<" has roll no 2"<<endl;}
 }
 void printMenu() {
 cout<<"*********************************************************************************************"<<endl;            
