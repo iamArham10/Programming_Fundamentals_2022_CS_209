92253b4436f51ce3153e51d80dc854906f5ae9b3
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-01-12 17:05:13 +0500

    Add files via upload

--- PF_Lab_06/Task_09.cpp
+++ PF_Lab_06/Task_09.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 using namespace std;
-float volleyball(string year,float holidays,float weekends);
+int volleyball(string year,float holidays,float weekends);
 
 main() {
     string leap_or_not;
     float holidays;
     float weekends; 
-    cout<<"Enter if this year is Leap or not";
+    cout<<"Enter if this year is Leap or not: ";
     cin >> leap_or_not;
     cout<<"Enter Number of holidays: ";
     cin >> holidays;
     cout <<"Enter Weekends: ";
     cin >> weekends;
      
-    cout<<"The number of times Vladmir has played volleyball is "<<volleyball(leap_or_not,holidays,weekends);
+    cout<<volleyball(leap_or_not,holidays,weekends);
 }
 
-float volleyball(string year,float holidays,float weekends) {
-    float first_part = (3/4)*48;
-    float second_part = holidays*(2/3);
-    float total = first_part+second_part;
+int volleyball(string year,float holidays,float weekends) {
+    float total = (3.0/4.0)*48.0+holidays*(2.0/3.0);
     float answer;
     if (year == "normal") {
         answer = total;
     }
     if (year == "leap") {
-       answer = (total*115)/100;
+       answer = (total*115.0)/100.0;
     }
-    return answer;
+   return answer;
 }
