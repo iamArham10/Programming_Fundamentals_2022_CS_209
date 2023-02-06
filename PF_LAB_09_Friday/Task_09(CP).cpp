f54bfc99bff42be97902da89d89e425bf5581e80
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-02-06 15:13:51 +0500

    added valid Option

--- PF_LAB_09_Friday/Task_09(CP).cpp
+++ PF_LAB_09_Friday/Task_09(CP).cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 using namespace std;
-main() {
-    int c;
-    string moves[10] = {"Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado","Pop", "Lock", "Arabesque"};
-    cout<<"Enter string of code of four digits: ";
-    cin >> c;
+bool valid_code(string value);
+void dance_move(int c);
+main()
+{
+
+    string value;
+    cout << "Enter string of code of four digits: ";
+    cin >> value;
+    if(valid_code(value) == false){
+        cout<<"Invalid Input";
+        return 0;
+    }
+    int c = stoi(value);
+    dance_move(c);
+    
+
+    
+}
+
+bool valid_code(string value)
+{
+
+    bool isValid = false;
+    for (int idx = 0; value[idx] != '\0'; idx++)
+    {
+        if (value[idx] == '0' || value[idx] == '1' || value[idx] == '2' || value[idx] == '3' || value[idx] == '4' || value[idx] == '5' || value[idx] == '6' || value[idx] == '7' || value[idx] == '8' || value[idx] == '9')
+        { isValid = true;
+        }
+        else {
+            isValid = false;
+            return isValid;
+        }
+    }
+    return isValid;
+}
+
+void dance_move(int c){
+    string moves[10] = {"Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado", "Pop", "Lock", "Arabesque"};
     int code[4];
     for (int idx = 3; idx > -1;idx--)
     {
@@ -17,11 +50,9 @@ main() {
         index = idx + code[idx];
         if (index > 9) {
             index = index - 10;
-        }  
+        }
         cout<<moves[index]<<" ";
 
     }
-    
 
-    
 }
