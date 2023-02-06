0e35a84cc511d1add80f2846d9d999c88560db6c
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-02-06 16:47:12 +0500

    Update Task_09(CP).cpp

--- PF_LAB_09_Friday/Task_09(CP).cpp
+++ PF_LAB_09_Friday/Task_09(CP).cpp
@@ -1,17 +1,21 @@
+// Create a program that converts a customer's PIN number to its dance equivalent. There is one dance move per digit in the PIN number.
+one dance move per digit in the PIN number.
 #include <iostream>
 using namespace std;
 bool valid_code(string value);
 void dance_move(int c);
 main()
 {
-
+// Taking string as input.
     string value;
     cout << "Enter string of code of four digits: ";
     cin >> value;
     if(valid_code(value) == false){
         cout<<"Invalid Input";
+//  Program will stop if string is invalid.
         return 0;
     }
+//  Converting string to integer.       
     int c = stoi(value);
     dance_move(c);
     
@@ -39,12 +43,15 @@ bool valid_code(string value)
 void dance_move(int c){
     string moves[10] = {"Shimmy", "Shake", "Pirouette", "Slide", "Box Step", "Headspin", "Dosado", "Pop", "Lock", "Arabesque"};
     int code[4];
+//     using for loop to separate each digit.    
     for (int idx = 3; idx > -1;idx--)
-    {
+    {   
+//      adding each digit to code array.
         code[idx] = c%10;
         c = c/10;
     }
     int index;
+//  print dance moves.
     for (int idx = 0; idx < 4;idx++)
     {
         index = idx + code[idx];
