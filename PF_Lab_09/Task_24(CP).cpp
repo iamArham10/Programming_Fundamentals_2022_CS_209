0f7586348fe072f1d32375a41a3ac87a6dae8053
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-02-06 17:03:08 +0500

    Update Task_24(CP).cpp

--- PF_Lab_09/Task_24(CP).cpp
+++ PF_Lab_09/Task_24(CP).cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
-main() {
+string novowel(string);
+main()
+{   
+    // Taking string as input
     string sentence;
     getline(cin, sentence);
-    string new_string;
-    for (int idx = 0; sentence[idx] != '\0';idx++)
-    {
-        if (sentence[idx] != 'a' && sentence[idx] != 'e' && sentence[idx] != 'i' && sentence[idx] != 'o' && sentence[idx] != 'u' )
-        {
-            cout<<sentence[idx];
-        }
+    cout<<novowel(sentence);
+}
 
+string novowel(string sentence)
+{   
+    // creating new character array
+    char new_string[100];
+    int index = 0;
+    // storing words that are not vowel in a new array
+    for (int idx = 0; sentence[idx] != '\0'; idx++)
+    {
+        if (sentence[idx] != 'a' && sentence[idx] != 'e' && sentence[idx] != 'i' && sentence[idx] != 'o' && sentence[idx] != 'u')
+        {
+            new_string[index] = sentence[idx];
+            index = index + 1;
+        }
     }
+    return new_string;
 }
