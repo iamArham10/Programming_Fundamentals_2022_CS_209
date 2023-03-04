c1cae5ca49d38efe83730019d40519dadd1b2abe
iamArham10 <103679355+iamArham10@users.noreply.github.com>
2023-03-04 09:14:48 +0500

    changed

--- Game/ninvaders/ninvaders.cpp
+++ Game/ninvaders/ninvaders.cpp
@@ -1,2 +1,43 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
+char My_Spaceship[2][3] = 
+    {
+        {214,214,214},
+        {212,212,212}
+    };
+void Print_Border();
+void Print_Spaceship();
+main()
+{
+    Print_Border();
+}
+
+void Print_Border()
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),4);
+    char c = '#';
+    system("cls");
+    for (int idx = 0;idx < 133;idx++)
+    {
+        cout << c;
+    }
+    cout << endl;
+    for (int idx = 0; idx < 30;idx++)
+    {
+        cout << c;
+        for(int idx = 0; idx < 132;idx++)
+        {
+            cout << " ";
+        }
+        cout << c << endl;
+    }
+    for (int idx = 0;idx < 133;idx++)
+    {
+        cout << c;
+    }
+}
+void Print_Spaceship()
+{
+    
+}
